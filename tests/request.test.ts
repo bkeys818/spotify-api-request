4f010062e39e0bb19377a057a9166f60fff42897
Ben Keys <bkeys818@gmail.com>
2021-06-16 10:53:01 -0400

    ✅ Update test

--- tests/request.test.ts
+++ tests/request.test.ts
@@ -1,49 +1,52 @@
-import { request, requestWithURL } from '../src/request'
+import { request } from '../src'
 import { SpotifyError } from '../src/error'
-// @ts-ignore
 import type { Globals } from '../jest.config'
 
 const token = (global as unknown as Globals).testData.token
 const albumID = '7gsWAHLeT0w7es6FofOXk1'
 
-
-test(`Function ${request.name} basic functionality`, async () => {
-    await expect(request('Get an Album', {
+test(`Function ${request.name} by name`, async () => {
+    const req = request({
+        name: 'Get an Album',
         token: token,
         pathParameter: {
-            "{id}": albumID
-        }
-    })).resolves.toHaveProperty('album_type', 'album')
+            '{id}': albumID,
+        },
+    })
+    await expect(req).resolves.toHaveProperty('album_type', 'album')
 })
 
-test(`Function ${requestWithURL.name} basic functionality`, async () => {
-    await expect(requestWithURL({
-        url: `https://api.spotify.com/v1/albums/${albumID}` as `https://api.spotify.com/v1/albums/${string}`,
-        method: 'GET',
-        token: token
-    })).resolves.toHaveProperty('album_type', 'album')
+test(`Function ${request.name} by url`, async () => {
+    const req = request({
+        url: `https://api.spotify.com/v1/albums/${albumID}`,
+        token: token,
+        method: 'GET'
+    })
+    await expect(req).resolves.toHaveProperty('album_type', 'album')
 })
 
 const messageFor = (type: SpotifyError['type']) => `SpotifyError - ${SpotifyError.Type[type]}`
 
 test.concurrent('Invalid token throws Unauthorized error', async () => {
-    const func = request('Get an Album', {
+    const req = request({
+        name: 'Get an Album',
         token: 'Basic fdnjksanjkfsajfjs',
         pathParameter: {
             "{id}": albumID
-        } 
+        }
     })
-    await expect(func).rejects.toThrowError(SpotifyError)
-    await expect(func).rejects.toThrowError(messageFor('authorization'))
+    await expect(req).rejects.toThrowError(SpotifyError)
+    await expect(req).rejects.toThrowError(messageFor('authorization'))
 })
 
 test.concurrent('Inavalid ID throws Bad Request', async () => {
-    const func = request('Get an Album', {
+    const req = request({
+        name: 'Get an Album',
         token: token,
         pathParameter: {
             "{id}": 'jkfre3345'
         }
     })
-    await expect(func).rejects.toThrowError(SpotifyError)
-    expect(func).rejects.toThrowError(messageFor('request'))
+    await expect(req).rejects.toThrowError(SpotifyError)
+    expect(req).rejects.toThrowError(messageFor('request'))
 })

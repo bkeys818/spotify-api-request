36c78c0320fb6db5c362034d63406e6dc2ebf96f
Ben Keys <bkeys818@gmail.com>
2021-06-25 18:28:20 -0400

    🚧 Write all artists functions

--- src/api/artists.ts
+++ src/api/artists.ts
@@ -1,1 +1,152 @@
-export {}
+import { sendRequest } from '../request'
+import type { Token } from '../authorize'
+import type { ArtistObject, TrackObject, AlbumObject, PagingObject } from './objects'
+
+/**
+ * Get Spotify catalog information for several artists based on their Spotify IDs.
+ * @param {Token} token - A valid user access token or your client credentials.
+ * @param {string[]} ids - A comma-separated list of the [Spotify IDs](https://developer.spotify.com/documentation/web-api/#spotify-uris-and-ids) for the artists. Maximum: 50 IDs.
+ * @returns {Promise<{ artists: (ArtistObject | null)[] }>} An object whose key is `"artists"` and whose value is an array of {@link ArtistObject artist object}.
+ */
+export async function getMultipleArtists(
+    token: Token,
+    ids: string[]
+): Promise<{
+    artists: (ArtistObject | null)[]
+}> {
+    return await (
+        await sendRequest({
+            endpoint: 'artists',
+            method: 'GET',
+            token: token,
+            queryParameter: {
+                ids: ids.join(','),
+            },
+        })
+    ).json()
+}
+
+/**
+ * Get Spotify catalog information for a single artist identified by their unique Spotify ID.
+ * @param {Token} token - A valid user access token or your client credentials.
+ * @param {string} id - The Spotify ID of the artist.
+ * @returns {Promise<ArtistObject>} An artist object.
+ */
+export async function getArtist(
+    token: Token,
+    id: string[]
+): Promise<ArtistObject> {
+    return await (
+        await sendRequest({
+            endpoint: 'artists/{id}',
+            method: 'GET',
+            token: token,
+            pathParameter: {
+                id: id,
+            },
+        })
+    ).json()
+}
+
+/**
+ * Get Spotify catalog information about an artist’s top tracks by country.
+ * @param {Token} token - A valid user access token or your client credentials.
+ * @param {string} id - The [Spotify ID](https://developer.spotify.com/documentation/web-api/#spotify-uris-and-ids) for the artist.
+ * @param {Object} [options]
+ * @param {string} options.market
+ * @returns {Promise<{ tracks: TrackObject[] }>} An object whose key is `"tracks"` and whose value is an array of up to 10 {@link TrackObject track objects}.
+ */
+export async function getArtistsTopTracks(
+    token: Token,
+    id: string,
+    options?: {
+        /** An [ISO 3166-1 alpha-2 country code](http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) or the string `from_token`. Synonym for `country`. */
+        market: string
+    }
+): Promise<{ tracks: TrackObject[] }> {
+    return await (
+        await sendRequest({
+            endpoint: 'artists/{id}/top-tracks',
+            method: 'GET',
+            token: token,
+            pathParameter: {
+                id: id,
+            },
+            queryParameter: options,  
+        })
+    ).json()
+}
+
+/**
+ * Get Spotify catalog information about artists similar to a given artist. Similarity is based on analysis of the Spotify community’s [listening history](http://news.spotify.com/se/2010/02/03/related-artists/).
+ * @param {Token} token - A valid user access token or your client credentials.
+ * @param {string} id - The [Spotify ID](https://developer.spotify.com/documentation/web-api/#spotify-uris-and-ids) for the artist.
+ * @returns {Promise<{ tracks: TrackObject[] }>} An object whose key is `"artists"` and whose value is an array of up to 20 {@link ArtistObject artist objects}.
+ */
+export async function getArtistsRelatedArtists(
+    token: Token,
+    id: string,
+): Promise<{ tracks: TrackObject[] }> {
+    return await (
+        await sendRequest({
+            endpoint: 'artists/{id}/related-artists',
+            method: 'GET',
+            token: token,
+            pathParameter: {
+                id: id,
+            },
+        })
+    ).json()
+}
+
+/**
+ * Get Spotify catalog information about an artist’s albums.
+ * @param {Token} token - A valid user access token or your client credentials.
+ * @param {string} id - The [Spotify ID](https://developer.spotify.com/documentation/web-api/#spotify-uris-and-ids) for the artist.
+ * @param {Object} [options]
+ * @param {string} [options.include_groups]
+ * @param {string} [options.market]
+ * @param {number} [options.limit]
+ * @param {number} [options.offset]
+ * @returns {Promise<PagingObject<AlbumObject>>} an array of simplified {@link AlbumObject album objects} (wrapped in a {@link PagingObject paging object}).
+ */
+export async function getArtistsAlbums(
+    token: Token,
+    id: string,
+    options?: {
+        /** 
+         * A comma-separated list of keywords that will be used to filter the response. If not supplied, all album types will be returned. Valid values are:
+         * - `album`
+         * - `single`
+         * - `appears_on`
+         * - `compilation`
+         * 
+         * For example: `include_groups=album,single.`
+         */
+        include_groups?: string
+        /** 
+         * Synonym for `country`. An [ISO 3166-1 alpha-2 country code](http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) or the string `from_token`.
+         * 
+         * Supply this parameter to limit the response to one particular geographical market. For example, for albums available in Sweden: `market=SE`.
+         * 
+         * *If not given, results will be returned for all markets and you are likely to get duplicate results per album, one for each market in which the album is available!*
+         */
+        market?: string
+        /** The number of album objects to return. Default: 20. Minimum: 1. Maximum: 50. */
+        limit?: number
+        /** The index of the first album to return. Default: 0 (i.e., the first album). Use with `limit` to get the next set of albums. */
+        offset?: number
+    }
+): Promise<PagingObject<AlbumObject>> {
+    return await (
+        await sendRequest({
+            endpoint: 'artists/{id}/albums',
+            method: 'GET',
+            token: token,
+            pathParameter: {
+                id: id,
+            },
+            queryParameter: options
+        })
+    ).json()
+}

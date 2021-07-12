060d69557c1d60d14ba38aee27809f76d2cc2059
Ben Keys <bkeys818@gmail.com>
2021-07-11 20:42:14 -0400

    ✅ Update searchForItem test
    Updates test for changes from the last commit. (5fdac6b7dc6e8305c260ec4c88b2063fdbe63f54)

--- tests/api/search.test.ts
+++ tests/api/search.test.ts
@@ -1,17 +1,17 @@
-import { searchforItem } from '../../src/api/search'
+import { searchForItem } from '../../src/api/search'
 import { pagingObject, trackObject } from './objects'
 
 // @ts-ignore
 const token = global.token
 
-test(searchforItem.name, async () => {
-    const res = await searchforItem(token, { q: 'heartbeat', type: 'track' })
+test(searchForItem.name, async () => {
+    const res = await searchForItem(token, { q: 'heartbeat', type: 'tracks' })
 
-    expect(res).toStrictEqual<typeof res>(
-        pagingObject({
-            value: res,
+    expect(res).toStrictEqual<typeof res>({
+        tracks: pagingObject({
+            value: res.tracks,
             endpoint: 'search',
-            testObj: trackObject,
+            testObj: trackObject
         })
-    )
+    })
 })

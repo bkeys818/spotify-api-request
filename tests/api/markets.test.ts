4f911288f6f8b23a45b5c6a0d8f16fec04f6f01f
Ben Keys <bkeys818@gmail.com>
2021-07-11 20:57:04 -0400

    ✅ Update getAvailableMarkets test
    Updates test for changes from the last commit. (fd83d8eceefef07819d7bff064fd98c846d59b89)

--- tests/api/markets.test.ts
+++ tests/api/markets.test.ts
@@ -6,8 +6,7 @@ const token = global.token
 test(getAvailableMarkets.name, async () => {
     const res = await getAvailableMarkets(token)
 
-    expect(res).toEqual(expect.any(Array))
-    res.forEach((market) => {
-        expect(market).toMatch(/[A-Z]{2}/)
+    expect(res).toStrictEqual<typeof res>({
+        markets: expect.arrayContaining([expect.stringMatching(/[A-Z]{2}/)]),
     })
 })

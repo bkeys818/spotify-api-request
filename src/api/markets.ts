fd83d8eceefef07819d7bff064fd98c846d59b89
Ben Keys <bkeys818@gmail.com>
2021-07-11 20:56:18 -0400

    🐛 Fix getAvailableMarkets return type

--- src/api/markets.ts
+++ src/api/markets.ts
@@ -4,11 +4,11 @@ import type { Token } from '../authorize'
 /**
  * Get the list of markets where Spotify is available.
  * @param {Token} token - A valid user access token or your client credentials.
- * @returns {Promise<string[]>} A list of the countries in which Spotify is available, identified by their [ISO 3166-1 alpha-2 country code](http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) with additional country codes for special territories.
+ * @returns {Promise<{ markets: string[] }>} A list of the countries in which Spotify is available, identified by their [ISO 3166-1 alpha-2 country code](http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) with additional country codes for special territories.
  */
 export async function getAvailableMarkets(
 	token: Token | string,
-): Promise<string[]> {
+): Promise<{ markets: string[] }> {
 	return await(
 		await sendRequest({
 			endpoint: 'markets',

71235a1ad10ce3fa14fab1721dbebc3f10452978
Ben Keys <bkeys818@gmail.com>
2021-07-12 21:59:44 -0400

    🚧 Test environmental variables values

--- tests/action.test.ts
+++ tests/action.test.ts
@@ -1,5 +1,6 @@
 export {}
 
 test('check variables', () => {
-    expect(process.env.CLIENT_ID).toBeTruthy()
+    expect(process.env.CLIENT_ID).toBe('TEST_VALUE_1')
+    expect(process.env.CLIENT_SECRET).toBe('TEST_VALUE_2')
 })

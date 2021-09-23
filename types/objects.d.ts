05a6f874c92735849c0951597c784613c4b8d538
Ben Keys <bkeys818@gmail.com>
2021-09-23 19:11:25 -0400

    🏷️ Update FollowersObject

--- types/objects.d.ts
+++ types/objects.d.ts
@@ -337,7 +337,7 @@ declare interface ExternalUrlObject {
 }
 declare interface FollowersObject {
     /** A link to the Web API endpoint providing full details of the followers; `null` if not available. Please note that this will always be set to null, as the Web API does not support it at the moment. */
-    href: string
+    href: string | null
     /** The total number of followers. */
     total: number;
 }
